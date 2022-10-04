46e54d715c2d85dd37b63fe0e1de52eb08d270eb
Camilla Jácome <camilla.cstrc@gmail.com>
2022-10-04 16:23:31 -0300

    resolve requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,10 @@
 const data = require('../data/zoo_data');
 
 function testParameter(scheduleTarget) {
+  // retorna se a entrada é um animal, dia, ou indefinida
+  if (scheduleTarget === undefined) {
+    return 'indefinido';
+  }
   const week = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
   if (data.species.some((animal) => scheduleTarget.includes(animal.name)) === true) {
     return 'animal';
@@ -8,46 +12,77 @@ function testParameter(scheduleTarget) {
   if (week.some((dia) => scheduleTarget.includes(dia)) === true) {
     return 'dia';
   }
-  if (scheduleTarget === undefined) {
-    return 'indefinido';
-  }
   return 'indefinido';
 }
 function animalAvailability(scheduleTarget) {
+  // retorna quais dias aquele animal esta disponível
   const busca = data.species.find((animal) => scheduleTarget.includes(animal.name));
   return busca.availability;
 }
 
-function animalDay(scheduleTarget) {
-  const animais = [];
+function animalAvailabilityDay(day) {
+  // retorna quais animais estão disponíveis naquele dia
+  const animals = [];
   data.species.forEach((element) => {
-    const busca = element.availability.some((animal) => animal === scheduleTarget);
-    if (busca === true) {
-      animais.push(element.name);
+    if (element.availability.some((dia) => day.includes(dia)) === true) {
+      animals.push(element.name);
     }
   });
-  return animais;
+  return animals;
 }
 
-/* function animalSchedule(scheduleTarget) {
-  if (testParameter(scheduleTarget) === 'animal') {
-    return animalAvailability(scheduleTarget);
-  }
-} */
 function getDay(scheduleTarget) {
-  const horas = data.hours;
-  console.log(Object.keys(horas));
+  // retorna os horários de funcionamento daquele dia
+  if (scheduleTarget === 'Monday') {
+    return 'CLOSED';
+  }
+  const { hours } = data;
+  const indice = Object.keys(hours).indexOf(scheduleTarget);
+  const horario = Object.entries(hours)[indice][1];
+  const resultado = `Open from ${horario.open}am until ${horario.close}pm`;
+  return resultado;
+}
+function getDaySchedule(scheduleTarget) {
+  if (scheduleTarget === 'Monday') {
+    return {
+      Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+    };
+  }
+  const horario = getDay(scheduleTarget);
+  const animais = animalAvailabilityDay(scheduleTarget);
+
+  const result = {
+    scheduleTarget: {
+      officeHour: horario,
+      exhibition: animais,
+    },
+  };
+
+  result[scheduleTarget] = result.scheduleTarget;
+  delete result.scheduleTarget;
+  return result;
+}
+
+function getAlltSchedule() {
+  const week = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+  let resultado = {};
+  week.forEach((element) => {
+    resultado = { ...resultado, ...getDaySchedule(element) };
+  });
+  return resultado;
 }
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  // retorna dias que o animal estará em exibição
   if (testParameter(scheduleTarget) === 'animal') {
     return animalAvailability(scheduleTarget);
   }
+  if (testParameter(scheduleTarget) === 'dia') {
+    return getDaySchedule(scheduleTarget);
+  }
+  if (testParameter(scheduleTarget) === 'indefinido') {
+    getAlltSchedule();
+  }
+  return getAlltSchedule();
 }
-// console.log(animalSchedule('lions'));
-console.log(getSchedule('lions'));
-console.log(testParameter('Friday'));
-console.log(animalDay('Friday'));
-getDay(' ');
 
 module.exports = getSchedule;
